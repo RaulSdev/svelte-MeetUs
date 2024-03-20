e8131f2b28e96a67c1582ace4d84df474b4d55de
Raul Samperio <raulsdev@gmail.com>
2024-03-20 15:30:30 -0600

    added btn styles and finshied form

--- meetus/src/routes/+page.svelte
+++ meetus/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
   import Header from '../UI/Header.svelte';
   import MeetupGrid from '../Meetups/MeetupGrid.svelte';
+  import TextInput from '../UI/TextInput.svelte';
+  import Button from '../UI/Button.svelte';
 
-  let title='';
-  let subtitle='';
-  let address='';
-  let email='';
-  let description='';
-  let imageUrl='';
+  let title = '';
+  let subtitle = '';
+  let address = '';
+  let email = '';
+  let description = '';
+  let imageUrl = '';
 
   let meetups = [
     {
@@ -34,14 +36,14 @@
   ];
 
   function addMeetup() {
-    const newMeetup ={
+    const newMeetup = {
       id: Math.random().toString(),
       title: title,
       subtitle: subtitle,
       description: description,
       imageUrl: imageUrl,
       contactEmail: email,
-      address: address
+      address: address,
     };
     meetups = [newMeetup, ...meetups];
   }
@@ -49,33 +51,51 @@
 
 <Header />
 
-<main >
-  <form on:submit|preventDefault="{addMeetup}">
-    <div class="form-control">
-    <label for="title">Title</label>
-    <input type="text" id="title" bind:value={title}/>
-  </div>
-    <div class="form-control">
-    <label for="subtitle">Subtitle</label>
-    <input type="text" id="subtitle" bind:value={subtitle}/>
-    </div>
-    <div class="form-control">
-    <label for="address">Address</label>
-    <input type="text" id="address" bind:value={address} />
-    </div>
-    <div class="form-control">
-    <label for="imageUrl">Image URL</label>
-    <input type="text" id="imageUrl" bind:value={imageUrl}/>
-    </div>
-    <div class="form-control">
-    <label for="email">E-mail</label>
-    <input type="email" id="email" bind:value={email} />
-    </div>
-    <div class="form-control">
-    <label for="description">Description</label>
-    <textarea rows="3"  id="description" bind:value={description} />
-    </div>
-    <button type="submit">Save</button>
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={(event) => (title = event.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      type="text"
+      value={subtitle}
+      on:input={(event) => (subtitle = event.target.value)}
+    />
+    <TextInput
+      id="address"
+      label="Address"
+      type="text"
+      value={address}
+      on:input={(event) => (address = event.target.value)}
+    />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      type="text"
+      value={imageUrl}
+      on:input={(event) => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      value={email}
+      type={email}
+      on:input={(event) => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description}
+      on:input={(event) => (description = event.target.value)}
+    />
+    <Button type="submit" caption="Save" />
   </form>
   <MeetupGrid {meetups} />
 </main>
@@ -83,5 +103,11 @@
 <style>
   main {
     margin-top: 5rem;
+  }
+
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
   }
 </style>
